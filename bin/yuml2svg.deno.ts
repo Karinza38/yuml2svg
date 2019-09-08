00644679458bc9c4f1d5276c046471335d9d1fbd
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-09-08 18:37:01 -0400

    Add warning when using TTY on deno

--- bin/yuml2svg.deno.ts
+++ bin/yuml2svg.deno.ts
@@ -3,18 +3,24 @@
 import yuml2svg from "../index.mjs";
 
 if (import.meta.main) {
-  const isDark = Deno.args[1] === "--dark";
-  Deno.readAll(Deno.stdin)
-    .then(buffer =>
-      yuml2svg(
-        buffer,
-        { isDark },
-        { workerURL: "https://dev.jspm.io/viz.js@2.1.2/full.render.js" },
-        {}
+  if (Deno.isTTY().stdin) {
+    console.log("Usage: cat diagram.yuml | yuml2svg > diagram.svg");
+    console.log("\tTakes yUML diagram via stdin and outputs SVG to stdout.");
+    console.log("\tOption: --dark to generate SVG with dark mode.");
+  } else {
+    const isDark = Deno.args[1] === "--dark";
+    Deno.readAll(Deno.stdin)
+      .then(buffer =>
+        yuml2svg(
+          buffer,
+          { isDark },
+          { workerURL: "https://dev.jspm.io/viz.js@2.1.2/full.render.js" },
+          {}
+        )
       )
-    )
-    .then(console.log)
-    .catch(console.error);
+      .then(console.log)
+      .catch(console.error);
+  }
 }
 
 export default yuml2svg;
