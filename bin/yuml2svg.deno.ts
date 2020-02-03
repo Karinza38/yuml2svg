06cbdee9814032629f1ab4214ee7fe8b9b97cf3d
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2020-02-03 15:32:31 +0100

    Remove absolute URL from deno file

--- bin/yuml2svg.deno.ts
+++ bin/yuml2svg.deno.ts
@@ -11,12 +11,7 @@ if (import.meta.main) {
     const isDark = Deno.args[1] === "--dark";
     Deno.readAll(Deno.stdin)
       .then(buffer =>
-        yuml2svg(
-          buffer,
-          { isDark },
-          { workerURL: "https://dev.jspm.io/viz.js@2.1.2/full.render.js" },
-          {}
-        )
+        yuml2svg(buffer, { isDark }, { workerURL: "@aduh95/viz.js/worker" }, {})
       )
       .then(console.log)
       .catch(console.error);
